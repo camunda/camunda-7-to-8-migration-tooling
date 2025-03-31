11b15c70c914da821b3b32dc1c4c6283b7866686
Bernd Ruecker <bernd.ruecker@camunda.com>
2025-03-31 09:17:40 +0200

    Ckeanup of unused components (related to https://github.com/camunda-community-hub/camunda-7-to-8-migration/issues/1164)

--- model-converter/cawemo-to-web-modeler-migration/go.mod
+++
@@ -1,3 +0,0 @@
-module cawemo-to-web-modeler-migration
-
-go 1.22.2
--- model-converter/code-migration-detector/core/src/test/java/org/camunda/community/migration/detector/rules/test/MyProcessEnginePlugin.java
+++
@@ -1,5 +0,0 @@
-package org.camunda.community.migration.detector.rules.test;
-
-import org.camunda.bpm.engine.impl.cfg.AbstractProcessEnginePlugin;
-
-public class MyProcessEnginePlugin extends AbstractProcessEnginePlugin {}
