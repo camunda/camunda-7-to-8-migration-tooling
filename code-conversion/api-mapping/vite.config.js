56069a4b46c9ee8dcfa0809859f02fcfad22feea
Till Stadtler <till.stadtler@camunda.com>
2025-04-11 10:06:05 +0200

    try new action

--- code-conversion/api-mapping/vite.config.js
+++ code-conversion/api-mapping/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/camunda-7-to-8-code-conversion/api-mapping",
+	base: "/camunda-7-to-8-code-conversion",
 });
