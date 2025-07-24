6cf46864480438a1508d6c24f56c3a31e17115ed
JensBrauckhoff <150697791+JensBrauckhoff@users.noreply.github.com>
2025-07-24 10:15:50 +0200

    Adapt examples from Jens from ilume
    * Example with a process starting an event sub process via delegate using message correlation
    
    * Example with a delegate using SPIN-Json to convert a JSON process variable to a POJO (I used a record for simplicity of the example)

---
+++ code-conversion/examples/process-solution-camunda-7/src/main/java/org/camunda/community/migration/example/SampleMessageStartEvent.java
@@ -0,0 +1,29 @@
+package de.ilume.jens.delegates;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component("SampleMessageStartEvent")
+public class SampleMessageStartEvent implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) {
+        final String messageName = (String) execution.getVariable("messageName");
+        final String processInstanceId = execution.getProcessInstanceId();
+        final String parameter = (String) execution.getVariable("parameter");
+
+        RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+        final Map<String, Object> processVariables = Map.of("parameter", parameter);
+
+        final List<MessageCorrelationResult> triggeredProcesses =
+                runtimeService.createMessageCorrelation(messageName)
+                        .processInstanceId(processInstanceId)
+                        .setVariables(processVariables)
+                        .correlateAllWithResult();
+    }
+}
