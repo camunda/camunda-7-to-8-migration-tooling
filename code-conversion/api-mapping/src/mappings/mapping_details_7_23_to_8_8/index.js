cf255c8a3e60c053aae646138676f73ebe97da50
gergely.juhasz@camunda.com <gergely.juhasz@camunda.com>
2025-11-25 14:54:25 +0100

    chore(code-conversion): FE add license header

--- code-conversion/api-mapping/src/mappings/mapping_details_7_23_to_8_8/index.js
+++ code-conversion/api-mapping/src/mappings/mapping_details_7_23_to_8_8/index.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
 import { authorization as authorization } from "./authorization";
 import { batch as batch } from "./batch";
 import { condition as condition } from "./condition";
