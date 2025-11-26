63239e1e18b8b4063b18ee20b90226dd24d7bf1f
gergely.juhasz@camunda.com <gergely.juhasz@camunda.com>
2025-11-26 08:49:15 +0100

    chore(code-conversion): consolidate GH Pages deployment

--- code-conversion/api-mapping/vite.config.js
+++ code-conversion/api-mapping/vite.config.js
@@ -11,5 +11,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/camunda-7-to-8-code-conversion",
+	base: "/camunda-7-to-8-migration-tooling/",
 });
