9e2f4f5575dd27bed7b24a6fdbb57f1fb08291d9
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2025-12-04 14:43:36 +0100

    wip

--- data-migrator/core/src/main/java/io/camunda/migrator/converter/ProcessDefinitionConverter.java
+++ data-migrator/core/src/main/java/io/camunda/migrator/converter/ProcessDefinitionConverter.java
@@ -9,14 +9,8 @@ package io.camunda.migrator.converter;
 
 import io.camunda.db.rdbms.write.domain.ProcessDefinitionDbModel;
 import io.camunda.migrator.impl.clients.C7Client;
-import io.camunda.migrator.impl.logging.ProcessDefinitionConverterLogs;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 import static io.camunda.migrator.impl.util.ConverterUtil.getNextKey;
 
@@ -26,11 +20,15 @@ public class ProcessDefinitionConverter {
   protected C7Client c7Client;
 
   public ProcessDefinitionDbModel apply(ProcessDefinition c7ProcessDefinition) {
-    String bpmnXml = getBpmnXmlAsString(c7ProcessDefinition);
+
+    String deploymentId = c7ProcessDefinition.getDeploymentId();
+    String resourceName = c7ProcessDefinition.getResourceName();
+
+    String bpmnXml = c7Client.getResourceAsString(deploymentId, resourceName);
 
     return new ProcessDefinitionDbModel.ProcessDefinitionDbModelBuilder().processDefinitionKey(getNextKey())
         .processDefinitionId(c7ProcessDefinition.getKey())
-        .resourceName(c7ProcessDefinition.getResourceName())
+        .resourceName(resourceName)
         .name(c7ProcessDefinition.getName())
         .tenantId(c7ProcessDefinition.getTenantId())
         .versionTag(c7ProcessDefinition.getVersionTag())
@@ -40,26 +38,4 @@ public class ProcessDefinitionConverter {
         .build();
   }
 
-  protected String getBpmnXmlAsString(ProcessDefinition processDefinition) {
-    try {
-      var resourceStream = c7Client.getResourceAsStream(processDefinition.getDeploymentId(),
-          processDefinition.getResourceName());
-
-      return readInputStreamToString(resourceStream);
-    } catch (IOException e) {
-      ProcessDefinitionConverterLogs.failedFetchingResourceStream(processDefinition.getId(), e.getMessage());
-      return null;
-    }
-  }
-
-  protected String readInputStreamToString(InputStream inputStream) throws IOException {
-    ByteArrayOutputStream result = new ByteArrayOutputStream();
-    byte[] buffer = new byte[1024];
-
-    for (int length; (length = inputStream.read(buffer)) != -1; ) {
-      result.write(buffer, 0, length);
-    }
-
-    return result.toString(StandardCharsets.UTF_8);
-  }
 }
