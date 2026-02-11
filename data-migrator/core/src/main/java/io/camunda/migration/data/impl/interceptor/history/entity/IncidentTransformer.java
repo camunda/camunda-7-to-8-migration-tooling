b1c0c200981cd44f937d897fb69a78b513771b2d
Joaquín <joaquin.felici@camunda.com>
2026-02-11 13:49:29 +0100

    chore(history): complete incident entities

--- data-migrator/core/src/main/java/io/camunda/migration/data/impl/interceptor/history/entity/IncidentTransformer.java
+++ data-migrator/core/src/main/java/io/camunda/migration/data/impl/interceptor/history/entity/IncidentTransformer.java
@@ -8,16 +8,20 @@
 package io.camunda.migration.data.impl.interceptor.history.entity;
 
 import static io.camunda.db.rdbms.write.domain.IncidentDbModel.Builder;
+import static io.camunda.migration.data.constants.MigratorConstants.C7_HISTORY_PARTITION_ID;
 import static io.camunda.migration.data.impl.util.ConverterUtil.convertDate;
 import static io.camunda.migration.data.impl.util.ConverterUtil.getNextKey;
 import static io.camunda.migration.data.impl.util.ConverterUtil.getTenantId;
 import static io.camunda.migration.data.impl.util.ConverterUtil.prefixDefinitionId;
-import static io.camunda.search.entities.IncidentEntity.IncidentState.RESOLVED;
 
+import io.camunda.migration.data.exception.EntityInterceptorException;
 import io.camunda.migration.data.interceptor.EntityInterceptor;
+import io.camunda.search.entities.IncidentEntity;
 import org.camunda.bpm.engine.history.HistoricIncident;
 
 import java.util.Set;
+import org.camunda.bpm.engine.history.IncidentState;
+import org.camunda.bpm.engine.impl.history.event.HistoricIncidentEventEntity;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -38,10 +42,24 @@ public class IncidentTransformer implements EntityInterceptor<HistoricIncident, 
         .errorType(null) // TODO: does error type exist in C7?
         .errorMessage(entity.getIncidentMessage())
         .creationDate(convertDate(entity.getCreateTime()))
-        .state(RESOLVED) // Mark incident always as resolved
-        .treePath(null) //TODO ?
+        .treePath(null)
+        .errorMessageHash(null)
+        .partitionId(C7_HISTORY_PARTITION_ID)
+        .jobKey(null)
+        .state(getIncidentState((HistoricIncidentEventEntity) entity))
         .tenantId(getTenantId(entity.getTenantId()));
-    // Note: processDefinitionKey, processInstanceKey, jobKey, and flowNodeInstanceKey are set externally
+        // Note: processDefinitionKey, processInstanceKey, jobKey, and flowNodeInstanceKey are set externally
+  }
+
+  private static IncidentEntity.IncidentState getIncidentState(HistoricIncidentEventEntity entity) {
+    int state = entity.getIncidentState();
+    if (state == IncidentState.DEFAULT.getStateCode()) {
+      return IncidentEntity.IncidentState.ACTIVE;
+    } else if (state == IncidentState.RESOLVED.getStateCode() || state == IncidentState.DELETED.getStateCode()) {
+      return IncidentEntity.IncidentState.RESOLVED;
+    } else {
+      throw new EntityInterceptorException("Could not determine incident state for state code: " + state);
+    }
   }
 
 }
