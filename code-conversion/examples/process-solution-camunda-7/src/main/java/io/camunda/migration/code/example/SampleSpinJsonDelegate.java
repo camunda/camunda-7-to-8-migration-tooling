8c5752b71429ddab32abc3c51521f3817bdc59d2
gergely.juhasz@camunda.com <gergely.juhasz@camunda.com>
2026-02-19 16:05:17 +0100

    WIP

--- code-conversion/examples/process-solution-camunda-7/src/main/java/io/camunda/migration/code/example/SampleSpinJsonDelegate.java
+++ code-conversion/examples/process-solution-camunda-7/src/main/java/io/camunda/migration/code/example/SampleSpinJsonDelegate.java
@@ -7,35 +7,29 @@
  */
 package io.camunda.migration.code.example;
 
-import io.camunda.client.api.response.ActivatedJob;
-import io.camunda.spring.client.annotation.JobWorker;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.spin.json.SpinJsonNode;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.camunda.spin.Spin.JSON;
 
-public class SampleSpinJsonDelegate {
+public class SampleSpinJsonDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) {
+        // the following 2 lines should be transformed to
+        // final Bar bar = (Bar.class) execution.getVariable("jsonVar");
+        final SpinJsonNode jsonVarInput = (SpinJsonNode) execution.getVariable("jsonVar");
+        final Bar bar = jsonVarInput.mapTo(Bar.class);
+        
+        Foo foo = new Foo(bar.bar1(), bar.bar2(), bar.bar3(), bar.bar4(), bar);
+
+        // the following two lines should be transformed to
+        // execution.setVariable("jsonVar", foo);
+        final SpinJsonNode jsonVarOutput = JSON(foo);
+        execution.setVariable("jsonVar", jsonVarOutput);
+    }
 
     public record Foo(String foo1, int foo2, boolean foo3, double foo4, Bar foo5) { }
 
     public record Bar(String bar1, int bar2, boolean bar3, double bar4) { }
-
-    @JobWorker(type = "sampleSpinJsonDelegate", autoComplete = true)
-    public Map<String, Object> executeJobMigrated(ActivatedJob job) throws Exception {
-        Map<String, Object> resultMap = new HashMap<>();
-        // the following 2 lines should be transformed to
-        // final Bar bar = (Bar.class) execution.getVariable("jsonVar");
-        final SpinJsonNode jsonVarInput = (SpinJsonNode) job.getVariable("jsonVar");
-        final Bar bar = jsonVarInput.mapTo(Bar.class);
-        
-        Foo foo = new Foo(bar.bar1(), bar.bar2(), bar.bar3(), bar.bar4(), bar);
-        
-        // the following two lines should be transformed to
-        // execution.setVariable("jsonVar", foo);
-        final SpinJsonNode jsonVarOutput = JSON(foo);
-        resultMap.put("jsonVar", jsonVarOutput);
-        return resultMap;
-    }
 }
