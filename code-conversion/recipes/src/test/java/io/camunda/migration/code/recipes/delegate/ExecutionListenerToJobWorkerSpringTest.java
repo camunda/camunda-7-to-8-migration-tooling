59a63a4a0bc5dd3f0e3601f43ac3619651b49554
gergely.juhasz@camunda.com <gergely.juhasz@camunda.com>
2026-02-24 15:31:15 +0100

    fix(code-conversion): add CleanupExecutionListenerRecipe

--- code-conversion/recipes/src/test/java/io/camunda/migration/code/recipes/delegate/ExecutionListenerToJobWorkerSpringTest.java
+++ code-conversion/recipes/src/test/java/io/camunda/migration/code/recipes/delegate/ExecutionListenerToJobWorkerSpringTest.java
@@ -5,29 +5,28 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.migration.code.recipes.delegate.prepare;
+package io.camunda.migration.code.recipes.delegate;
 
 import static org.openrewrite.java.Assertions.java;
 
-import io.camunda.migration.code.recipes.delegate.PrepareJobWorkerBeneathExecutionListenerRecipe;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-class InsertJobWorkerForExecutionListenerTest implements RewriteTest {
+class ExecutionListenerToJobWorkerSpringTest implements RewriteTest {
 
-  @Override
-  public void defaults(RecipeSpec spec) {
-    spec.recipes(new PrepareJobWorkerBeneathExecutionListenerRecipe())
+    @Override
+    public void defaults(RecipeSpec spec) {
+    spec.recipeFromResources("io.camunda.migration.code.recipes.AllDelegateRecipes")
         .parser(JavaParser.fromJavaVersion().classpath(JavaParser.runtimeClasspath()));
-  }
+    }
 
-  @Test
-  void injectJobWorkerForExecutionListener() {
+    @Test
+    void rewriteExecutionListener() {
     rewriteRun(
         java(
-            """
+"""
 package org.camunda.conversion.execution_listeners;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -42,33 +41,28 @@ public class MyExecutionListener implements ExecutionListener {
         String someVar = (String) execution.getVariable("foo");
     }
 }
-            """,
-            """
+""",
+"""
 package org.camunda.conversion.execution_listeners;
 
 import io.camunda.client.annotation.JobWorker;
 import io.camunda.client.api.response.ActivatedJob;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class MyExecutionListener implements ExecutionListener {
-
-    @Override
-    public void notify(DelegateExecution execution) {
-        String someVar = (String) execution.getVariable("foo");
-    }
+public class MyExecutionListener {
 
     @JobWorker(type = "myExecutionListener", autoComplete = true)
-    public Map<String, Object> executeJob(ActivatedJob job) throws Exception {
+    public Map<String, Object> executeJobMigrated(ActivatedJob job) throws Exception {
         Map<String, Object> resultMap = new HashMap<>();
+        String someVar = (String) job.getVariable("foo");
         return resultMap;
     }
 }
-            """));
-  }
+"""));
+    }
 }
+
--- code-conversion/recipes/src/test/java/io/camunda/migration/code/recipes/delegate/cleanup/RemoveExecutionListenerTest.java
+++ code-conversion/recipes/src/test/java/io/camunda/migration/code/recipes/delegate/cleanup/RemoveExecutionListenerTest.java
@@ -5,45 +5,22 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.migration.code.recipes.delegate.prepare;
+package io.camunda.migration.code.recipes.delegate.cleanup;
 
 import static org.openrewrite.java.Assertions.java;
 
-import io.camunda.migration.code.recipes.delegate.PrepareJobWorkerBeneathExecutionListenerRecipe;
+import io.camunda.migration.code.recipes.delegate.CleanupExecutionListenerRecipe;
 import org.junit.jupiter.api.Test;
-import org.openrewrite.java.JavaParser;
-import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-class InsertJobWorkerForExecutionListenerTest implements RewriteTest {
-
-  @Override
-  public void defaults(RecipeSpec spec) {
-    spec.recipes(new PrepareJobWorkerBeneathExecutionListenerRecipe())
-        .parser(JavaParser.fromJavaVersion().classpath(JavaParser.runtimeClasspath()));
-  }
+class RemoveExecutionListenerTest implements RewriteTest {
 
   @Test
-  void injectJobWorkerForExecutionListener() {
+  void removeExecutionListenerTest() {
     rewriteRun(
+        spec -> spec.recipe(new CleanupExecutionListenerRecipe()),
         java(
-            """
-package org.camunda.conversion.execution_listeners;
-
-import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.ExecutionListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MyExecutionListener implements ExecutionListener {
-
-    @Override
-    public void notify(DelegateExecution execution) {
-        String someVar = (String) execution.getVariable("foo");
-    }
-}
-            """,
-            """
+"""
 package org.camunda.conversion.execution_listeners;
 
 import io.camunda.client.annotation.JobWorker;
@@ -69,6 +46,27 @@ public class MyExecutionListener implements ExecutionListener {
         return resultMap;
     }
 }
-            """));
+""",
+"""
+package org.camunda.conversion.execution_listeners;
+
+import io.camunda.client.annotation.JobWorker;
+import io.camunda.client.api.response.ActivatedJob;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MyExecutionListener {
+
+    @JobWorker(type = "myExecutionListener", autoComplete = true)
+    public Map<String, Object> executeJob(ActivatedJob job) throws Exception {
+        Map<String, Object> resultMap = new HashMap<>();
+        return resultMap;
+    }
+}
+"""));
   }
 }
+
