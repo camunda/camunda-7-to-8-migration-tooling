4f9ae6d1a750783f206b183737f3403dcbaeed5e
Joaquín <joaquin.felici@camunda.com>
2026-03-19 09:41:51 +0100

    squash: temporarily disable user migration

--- data-migrator/core/src/main/java/io/camunda/migration/data/IdentityMigrator.java
+++ data-migrator/core/src/main/java/io/camunda/migration/data/IdentityMigrator.java
@@ -76,7 +76,7 @@ public class IdentityMigrator {
   }
 
   protected void migrate() {
-    migrateUsers();
+//    migrateUsers();
     migrateGroups();
     migrateTenants();
     migrateAuthorizations();
