86ee19d0be67d1e808430fcfcd7902e4fb2f5e8f
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2026-05-19 11:49:45 +0200

    fix(data-migrator): fix resolution of `databaseId`

--- data-migrator/core/src/main/java/io/camunda/migration/data/config/mybatis/C8Configuration.java
+++ data-migrator/core/src/main/java/io/camunda/migration/data/config/mybatis/C8Configuration.java
@@ -7,8 +7,6 @@
  */
 package io.camunda.migration.data.config.mybatis;
 
-import static io.camunda.db.rdbms.config.VendorDatabaseProperties.DATABASE_ID;
-
 import io.camunda.client.metrics.MetricsRecorder;
 import io.camunda.db.rdbms.config.VendorDatabaseProperties;
 import io.camunda.db.rdbms.read.RdbmsReaderConfig;
@@ -138,7 +136,7 @@ public class C8Configuration extends AbstractConfiguration {
     String c8DbVendor = dbVendorProvider.getDatabaseId(getC8DataSource());
     String c8File = "db/vendor-properties/" + c8DbVendor + ".properties";
     Properties properties = loadPropertiesFile(c8DbVendor, c8File);
-    properties.put(DATABASE_ID, c8DbVendor);
+    properties.put("databaseId", c8DbVendor);
     return new VendorDatabaseProperties(properties);
   }
 
